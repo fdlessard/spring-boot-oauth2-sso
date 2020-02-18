7ff897c316cd9ac9d91b6f830cc603a04b6b37b8
Francois-David Lessard <francois-david.lessard@nbc.ca>
2020-02-18 14:53:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# spring-boot-oauth2-sso
+
+https://docs.spring.io/spring-security/site/docs/current/reference/html/oauth2.html
+https://developer.okta.com/blog/2019/05/15/spring-boot-login-options
---
+++ src/test/java/io/fdlessard/codebites/oauth2/sso/SpringBootOauth2SsoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fdlessard.codebites.oauth2.sso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2SsoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
